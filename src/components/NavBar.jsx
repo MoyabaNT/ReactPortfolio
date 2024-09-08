081dd514b6baf899519d8256bbd560d2987398cb
MoyabaNT <155981478+MoyabaNT@users.noreply.github.com>
2024-09-08 19:57:15 +0200

    navbar color changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -57,8 +57,8 @@ const NavBar = () => {
 
       {nav && (
  <ul className="flex flex-col justify-center items-center 
-  absolute top-0 left-0 w-full h-screen bg-gradient-to-b 
-   from-black to-gray-800 text-gray-500">
+  absolute top-0 right-0 py-4 w-96 h-92 bg-gradient-to-b 
+   from-black via-gray-800 to-black text-gray-500 rounded-lg">
    {links.map(({id, link}) =>(
        <li 
        key={link.id}
